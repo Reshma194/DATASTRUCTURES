015da42f8a535fa209d310f3984b6b4cf6875342
Reshma194 <reshmacharmer@gmail.com>
2025-02-09 18:21:24 +0530

    Initial commit

---
+++ src/Search2DArray.java
@@ -0,0 +1,18 @@
+public class Search2DArray {
+    public static void main(String[] args){
+        int[][] arr={
+            {23,4,1},
+            {18,12,6,4},
+                {78,99,43,56},
+                {18,12}
+        };
+        int target=6;
+        System.out.println(search(arr,target));
+    }
+     static int search(int[][] arr,int target){
+
+        return -1;
+
+    }
+
+}
